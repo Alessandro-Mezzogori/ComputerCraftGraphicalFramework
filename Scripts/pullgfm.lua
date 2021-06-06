78272baeeb1794f2ccd77ae317c69be25e02faf5
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2021-06-06 23:01:39 +0200

    [PullScript-1]

---
+++ Scripts/pullgfm.lua
@@ -0,0 +1,15 @@
+libs = {
+	{"drawing.lua"}, 
+	{"event_manager.lua"}, 
+	{"buttons.lua". "https://raw.githubusercontent.com/Alessandro-Mezzogori/GraphicalFramework/master/Libs/buttons.lua"}, 
+	{"custom_colors.lua"}, 
+	{"helper_functions.lua"}
+}
+
+programs = {
+
+}
+
+commands = {
+	{"pullgfm.lua"}
+}
