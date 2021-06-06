f2cf2e806b24e4b6f4086550fea4d00e938eaca7
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2021-06-06 23:07:14 +0200

    First Test [PullScript-2]

--- Scripts/pullgfm.lua
+++ Scripts/pullgfm.lua
@@ -1,15 +1,20 @@
 libs = {
-	{"drawing.lua"}, 
-	{"event_manager.lua"}, 
+	{"drawing.lua", "https://raw.githubusercontent.com/Alessandro-Mezzogori/GraphicalFramework/master/Libs/drawing.lua"}, 
+	{"event_manager.lua" ,"https://raw.githubusercontent.com/Alessandro-Mezzogori/GraphicalFramework/master/Libs/event_manager.lua"}, 
 	{"buttons.lua". "https://raw.githubusercontent.com/Alessandro-Mezzogori/GraphicalFramework/master/Libs/buttons.lua"}, 
-	{"custom_colors.lua"}, 
-	{"helper_functions.lua"}
+	{"custom_colors.lua", "https://raw.githubusercontent.com/Alessandro-Mezzogori/GraphicalFramework/master/Libs/custom_colors.lua"}, 
+	{"helper_functions.lua", "https://raw.githubusercontent.com/Alessandro-Mezzogori/GraphicalFramework/master/Libs/helper_functions.lua"}
 }
 
 programs = {
 
 }
 
-commands = {
-	{"pullgfm.lua"}
-}
+
+for _, element in ipairs(libs) do
+	os.execute("wget --no-check-certificate --content-disposition " .. element[2] .. " " .. arg[1] .. "/Lib/" .. element[1])
+end
+
+for _, element in ipairs(programs) do
+	os.execute("wget --no-check-certificate --content-disposition " .. element[2] .. " " .. arg[1] .. "/Lib/" .. element[1])
+end
