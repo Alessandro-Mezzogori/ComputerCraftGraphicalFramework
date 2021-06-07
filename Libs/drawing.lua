5ce1962ea08fa3c0062200232056bfd1eb615fc6
Alessandro Mezzogori <alessandro.mezzogori1802@gmail.com>
2021-06-07 23:46:51 +0200

    First test with elements and scenes [Drawing]

--- Libs/drawing.lua
+++ Libs/drawing.lua
@@ -11,8 +11,17 @@ local font_ratio = 21/12 -- height of font / width of font (at 12 pt Calibri)
 local drawingGPU = nil
 local runningEventLoop = false
 
-local scenes = {} -- table containing all the scenes 
-local currentScene = {} -- current displayed scene
+-- array of elements aka {<drawing function>, <table of params>} -> the id of the entry is the element id
+local elements = {
+	{drawingFunction=drawSquare, params={50, 20, 20, 0xFFFFFF}} -- one example element
+}
+
+-- table containing all the scenes, a scene is an array of element id's
+local scenes = {
+	{1}
+} 
+
+local currentScene = 1  -- current idnex of displayed scene
 
 --[[
   function to bind the passed gpu to be the drawing gpu  
@@ -37,7 +46,7 @@ end
   Note: the origin (0,0) is the upper-left corner of the screen
         color is an hex value 32 bit
 ]]--
-function drawing.drawRectangle(x, y, xSize, ySize, color)
+function drawRectangle(x, y, xSize, ySize, color)
   local saved_color = drawingGPU.getBackground()
   drawingGPU.setBackground(color)
   drawingGPU.fill(x, y, xSize, ySize*computeAspectRatio(), draw_char)
@@ -47,14 +56,23 @@ end
 --[[ 
   wrapper for drawRectangle with same side size
 ]]--
-function drawing.drawSquare(x, y, size, color)
+function drawSquare(x, y, size, color)
   drawing.drawRectangle(x, y, size, size, color)
 end
 
--- thread functions 
+--############### SCENE ###############--
+function redrawScene()
+	-- TODO add a clear screen function
+	-- for each element id call it's drawing function 
+	for _, elementID in ipairs(scenes[currentScene]) do
+		elements[elementID].drawingFunction(elements[elementID].params)
+	end
+end
+
+--############### THREAD ###############-- 
 function handleDrawingEvent(eventID, ...)
 	if eventID == "drawing" then
-		drawing.drawSquare(70, 20, 20, 0xFFFFFF)
+		redrawScene()
 	end
 end
 
@@ -63,13 +81,13 @@ function drawing.stopEventLoop()
 end
 
 function drawing.startEventLoop()
+	-- bind the current gpu to be the drawer
+	drawing.bindDrawingGPU(gpu)
+
 	runningEventLoop = true
 	while runningEventLoop do
 		handleDrawingEvent(event.pull()))
 	end
 end	
-
--- default initialization
-drawing.bindDrawingGPU(gpu)
 
 return drawing
